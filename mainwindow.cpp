857332a0e6febd246ed9e6f3f0f043246bd98963
sunsijie <sunsijie@buaa.edu.cn>
2017-06-03 16:49:41 +0800

    Export Import A Hisroty

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,59 @@
 #include <QDoubleSpinBox>
 #include <iostream>
 #include <QLineEdit>
+#include <QFileDialog>
+#include <fstream>
+
+void MainWindow::SetItemState(int x, int y, bool cellState)
+{
+    model->setItem(x, y, new QStandardItem(""));
+    model->item(x, y)->setBackground(QBrush(cellState ? aliveColor : dieColor));
+}
+
+int MainWindow::GetDimFromEdit()
+{
+    bool ok = false;
+    dim = ui->dimEdit->text().toInt(&ok);
+    if (!ok || dim <= 0)
+    {
+        dim = 10;
+        ui->dimEdit->setText(QString::number(dim));
+    }
+    return dim;
+}
+
+int MainWindow::GetHistoryLimitFromEdit()
+{
+    bool ok = false;
+    historyLimit = ui->historyLimit->text().toInt(&ok);
+    if (!ok || historyLimit <= 0)
+    {
+        historyLimit = 10;
+        ui->historyLimit->setText(QString::number(historyLimit));
+    }
+    return historyLimit;
+}
+
+void MainWindow::InitHistorySlider(int limit)
+{
+    ui->historySlide->setRange(1, limit);
+}
+
+void MainWindow::InitTableView(int dim)
+{
+    for (int i = 0; i < dim; i++)
+    {
+        for (int j = 0; j < dim ; j++)
+        {
+            SetItemState(i, j, false);
+        }
+    }
+    model->setColumnCount(dim);
+    model->setRowCount(dim);
+    modelMatrix = Matrix::MatrixType(dim, std::vector<bool>(dim));
+    m = std::shared_ptr<Matrix>(new Matrix(dim));
+    director->SetMatrix(m);
+}
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -15,28 +68,20 @@ MainWindow::MainWindow(QWidget *parent) :
     dieColor(255, 255, 255)
 {
     ui->setupUi(this);
-    Matrix::MatrixType matrix = {
-                         {1,0,1,0,1},
-                         {0,1,0,1,0},
-                         {1,0,1,0,1},
-                         {0,1,0,1,0},
-                         {1,0,1,0,1},
-                 };
-    m = std::shared_ptr<Matrix>(new Matrix(matrix.size()));
-    m->AssignInitState(matrix);
-    dim = m->getcurrentMatrix().size();
-
-    director->SetMatrix(m);
 
     model = new QStandardItemModel();
-    model->setColumnCount(dim);
-    model->setRowCount(dim);
     ui->tableView->setModel(model);
-
     ui->tableView->verticalHeader()->hide();
     ui->tableView->horizontalHeader()->hide();
     ui->tableView->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
     ui->tableView->verticalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    historyLimit = GetHistoryLimitFromEdit();
+    director->SetHistoryLimit(historyLimit);
+    InitHistorySlider(historyLimit);
+
+    dim = GetDimFromEdit();
+    InitTableView(dim);
 
     connect(timer, SIGNAL(timeout()), this, SLOT(updateMatrixView()));
 }
@@ -46,21 +91,19 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::test()
+void MainWindow::Update()
 {
     if (!curMatrix)
     {
         return;
     }
     for (int i = 0; i < dim; i++)
+    {
         for (int j = 0; j < dim ; j++)
         {
-            model->setItem(i,j, new QStandardItem(""));
-            if ((*curMatrix)[i][j])
-                model->item(i, j)->setBackground(QBrush(aliveColor));
-            else
-                model->item(i, j)->setBackground(QBrush(dieColor));
+            SetItemState(i, j, (*curMatrix)[i][j]);
         }
+    }
 }
 
 void MainWindow::on_pushButton_clicked()
@@ -70,14 +113,17 @@ void MainWindow::on_pushButton_clicked()
     ui->okButton->setEnabled(true);
     ui->clearButton->setEnabled(true);
     ui->setAsInitButton->setEnabled(true);
+    ui->historySlide->setEnabled(true);
     running = false;
     director->StopProducing();
+    ui->historySlide->setValue(director->GetHistorySize());
 }
 
 void MainWindow::updateMatrixView()
 {
     this->curMatrix = director->PopCurrentMatrix();
-    test();
+    InitHistorySlider(director->GetHistorySize());
+    Update();
     double speed = ui->speedSpin->value();
     timer->start((int)(speed * 1000));
 }
@@ -87,45 +133,14 @@ void MainWindow::on_tableView_clicked(const QModelIndex &index)
     if (running)
         return;
     int x = index.row(), y = index.column();
-    model->setItem(x, y, new QStandardItem(""));
-    if (modelMatrix[x][y])
-    {
-        model->item(x, y)->setBackground(QBrush(dieColor));
-    }
-    else
-    {
-        model->item(x, y)->setBackground(QBrush(aliveColor));
-    }
     modelMatrix[x][y] = !modelMatrix[x][y];
-}
-
-void MainWindow::on_pushButton_2_clicked()
-{
-    ui->okButton->setEnabled(false);
-    ui->clearButton->setEnabled(false);
-    ui->setAsInitButton->setEnabled(false);
-    timer->start(100);
-    running = true;
-    director->StartProducing();
+    SetItemState(x, y, modelMatrix[x][y]);
 }
 
 void MainWindow::on_okButton_clicked()
 {
-    bool ok = false;
-    dim = ui->dimEdit->text().toInt(&ok);
-    if (!ok)
-    {
-        dim = 10;
-        ui->dimEdit->setText(QString::number(dim));
-    }
-    model->setColumnCount(dim);
-    model->setRowCount(dim);
-
-    modelMatrix = Matrix::MatrixType(dim, std::vector<bool>(dim));
-    m = std::shared_ptr<Matrix>(new Matrix(dim));
-    m->AssignInitState(modelMatrix);
-    director->SetMatrix(m);
-
+    dim = GetDimFromEdit();
+    InitTableView(dim);
 }
 
 void MainWindow::on_setAsInitButton_clicked()
@@ -135,13 +150,54 @@ void MainWindow::on_setAsInitButton_clicked()
 
 void MainWindow::on_clearButton_clicked()
 {
-    for (int i = 0; i < dim; i++)
+    InitTableView(dim);
+}
+
+void MainWindow::on_startButton_clicked()
+{
+    ui->okButton->setEnabled(false);
+    ui->clearButton->setEnabled(false);
+    ui->setAsInitButton->setEnabled(false);
+    ui->historySlide->setEnabled(false);
+    timer->start(100);
+    running = true;
+    director->StartProducing();
+}
+
+void MainWindow::on_historySlide_valueChanged(int value)
+{
+    curMatrix = director->GetHistory(value - 1);
+    Update();
+}
+
+void MainWindow::on_historyLimit_textChanged(const QString &)
+{
+    historyLimit = GetHistoryLimitFromEdit();
+    director->SetHistoryLimit(historyLimit);
+}
+
+void MainWindow::on_exportButton_clicked()
+{
+    QString path = QFileDialog::getSaveFileName(this, tr("Open txt"), ".", tr("txt Files(*.txt)"));
+    if (path.length() != 0)
     {
-        for (int j = 0; j < dim ; j++)
-        {
-            model->setItem(i,j, new QStandardItem(""));
-            model->item(i, j)->setBackground(QBrush(dieColor));
-            modelMatrix[i][j] = false;
-        }
+        std::fstream fs(path.toStdString(), std::ios_base::trunc | std::ios_base::out);
+        if (curMatrix)
+            Matrix::Export(*curMatrix, fs);
+        else
+            Matrix::Export(modelMatrix, fs);
+        fs.close();
     }
+}
+
+void MainWindow::on_importButton_clicked()
+{
+    QString path = QFileDialog::getOpenFileName(this, tr("Open txt"), ".", tr("txt Files(*.txt)"));
+    if (path.length() != 0)
+    {
+        std::fstream fs(path.toStdString());
+        modelMatrix = Matrix::Import(fs);
+    }
+    curMatrix = &modelMatrix;
+    Update();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -14,8 +14,10 @@ class MainWindow;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+private:
+    Ui::MainWindow *ui;
     std::shared_ptr<Matrix> m;
-    Director* director = new Director(1, 1);
+    Director* director = new Director(10, 10);
     QTimer* timer = new QTimer();
     int dim;
     QStandardItemModel* model = nullptr;
@@ -24,16 +26,21 @@ class MainWindow : public QMainWindow
     bool running = false;
     QColor aliveColor;
     QColor dieColor;
+    int historyLimit;
+
+    void SetItemState(int x, int y, bool cellState);
+    void InitTableView(int dim);
+    int GetDimFromEdit();
+    int GetHistoryLimitFromEdit();
+    void InitHistorySlider(int limit);
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-    void test();
+    void Update();
 private slots:
     void on_pushButton_clicked();
     void updateMatrixView();
     void on_tableView_clicked(const QModelIndex &index);
-
-    void on_pushButton_2_clicked();
 
     void on_okButton_clicked();
 
@@ -41,8 +48,11 @@ private slots:
 
     void on_clearButton_clicked();
 
-private:
-    Ui::MainWindow *ui;
+    void on_startButton_clicked();
+    void on_historySlide_valueChanged(int value);
+    void on_historyLimit_textChanged(const QString &arg1);
+    void on_exportButton_clicked();
+    void on_importButton_clicked();
 };
 
 #endif // MAINWINDOW_H
